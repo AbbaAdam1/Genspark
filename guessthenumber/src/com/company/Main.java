f74c0fa685ffb8da42e84e47d7f49827673fb47c
AbbaAdam1 <90100486+AbbaAdam1@users.noreply.github.com>
2021-12-28 22:34:06 -0500

    Add files via upload

---
+++ guessthenumber/src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int turn = 1;
+        String loop = "";
+        String input = "";
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Hello! What is your name?");
+        String name = obj.nextLine();
+        System.out.println("Well, " + name + " I am thinking of a number between 1 and 20");
+        System.out.println("Take a guess.");
+        while (!input.equals("n")) {
+            double randomNumber = Math.random();
+            Random r = new Random();
+            int low = 1;
+            int high = 20;
+            int rng = r.nextInt(high - low) + low;
+            String Strng = Integer.toString(rng);
+            //System.out.println(rng);
+            input ="";
+
+            while (!Objects.equals(input, "y")) {
+                if (turn != 7){
+                    input = obj.nextLine();
+                    int answer = Integer.parseInt(input);
+                    if (answer > rng) {
+                        System.out.println("Your guess is too high.");
+                        turn++;
+                    } else if (answer < rng) {
+                        System.out.println("Your guess is too low.");
+                        turn++;
+                    } else {
+                        String attempts;
+                        attempts = Integer.toString(turn);
+                        System.out.println("Good job, " + name + ". You guessed my number in " + attempts + " guesses.");
+                        System.out.println("Would you like to try again? (y or n)");
+                        input = obj.nextLine();
+                        if (Objects.equals(input, "n")) {
+                            System.exit(0);
+                        }
+                        turn = 1;
+                        //int loop = Integer.parseInt(input);
+                        System.out.println("Take a guess.");
+                    }
+                }   else    {
+                    System.out.println("You took too many guesses!");
+                    System.out.println("Would you like to try again? (y or n)");
+                    input = obj.nextLine();
+                    if (Objects.equals(input, "n")) {
+                        System.exit(0);
+                    }
+                    turn = 1;
+                    System.out.println("Take a guess.");
+                }
+
+            }
+        }
+    }
+}
